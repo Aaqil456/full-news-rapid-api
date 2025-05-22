5bb62a8fc96ca00f4cc0028d4226fc9d2e111e11
Aaqil Ahamad <72489957+Aaqil456@users.noreply.github.com>
2025-05-22 23:59:49 +0800

    Update cryptonews_translator.py

--- cryptonews_translator.py
+++ cryptonews_translator.py
@@ -22,7 +22,9 @@ ALLOWED_NEWS_DOMAINS = [
     "cointelegraph.com",
     "bitcoinist.com",
     "decrypt.co",
-    "cryptodaily.co.uk"
+    "cryptodaily.co.uk",
+    "bitcoinethereumnews.com"
+    
 ]
 
 # === MAIN FUNCTION ===
